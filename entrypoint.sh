dff1893e1d7e44277cb996d94061ee67324c3ab0
Daniel Reuter <dreuter@kth.se>
2020-02-20 20:44:23 +0100

    fix syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ file_env() {
 	local fileVar="${var}_FILE"
 	local def="${2:-}"
 	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		echo "error: both $var and $fileVar are set (but are exclusive)" >&2 
 		exit 1
 	fi
 	local val="$def"
